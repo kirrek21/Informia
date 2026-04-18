77128974149f43fb65d1d3f2a11b2c07409e4cb1
Kirill <kirrekviem@gmail.com>
2026-04-18 17:52:53 +0300

    completed the eighth task

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Article;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\ArticleMail;
 
 class ArticleController extends Controller
 {
@@ -41,7 +43,8 @@ class ArticleController extends Controller
         $article->title = $request->title;
         $article->shortDesc = $request->shortDesc;
         $article->desc = $request->desc;
-        $article->save();
+        $result = $article->save();
+        if ($result) Mail::send(new ArticleMail($article));
         return redirect(route('article.index'));
     }
 
